f1e49e499bd3ca8d61f82b829d46078e6bd1694d
abdelrahmansafwat <bedo_aboraia@yahoo.com>
2021-11-14 12:28:08 +0200

    Modularized components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React, { useState } from "react";
-import VideoJS from './components/VideoJS'
+import Player from './components/Player/Player'
 
 const App = () => {
-  const playerRef = React.useRef(null);
-  const [options, setOptions] = useState({
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    sources: [{
-      src: 'https://s3.amazonaws.com/_bc_dml/example-content/sintel_dash/sintel_vod.mpd',
-      type: 'application/dash+xml'
-    }]
-  });
-
-  const handlePlayerReady = (player) => {
-    playerRef.current = player;
-
-    // you can handle player events here
-    player.on('waiting', () => {
-      console.log('player is waiting');
-    });
-
-    player.on('dispose', () => {
-      console.log('player will dispose');
-    });
-  };
-
   return (
     <>
-      <VideoJS options={options} onReady={handlePlayerReady} />
+      <Player />
     </>
   );
 }
